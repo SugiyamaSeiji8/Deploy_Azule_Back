b9986dd05fa2903a395b6ab8aa38af4cadba4d97
seiji sugiyama <seiji_sugiyama@risebranding.jp>
2024-12-08 17:39:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+
 CORS(app, resources=
-     {r"/api/*": {"origins": ["https://tech0-gen-8-step3-testapp-node1-8.azurewebsites.net","http://localhost:3000" # ローカル開発⽤]
-}
+     {r"/api/*": {
+          "origins": [
+               "https://tech0-gen-8-step3-testapp-node1-8.azurewebsites.net",
+               "http://localhost:3000" # ローカル開発⽤
+ 　　　　              ]
+                 }
 })
 
 @app.route('/', methods=['GET'])
